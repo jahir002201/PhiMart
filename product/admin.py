8154d8427004ec7b8a8171abe3b707877bf7f892
Md. Jahirul Islam <90275453+jahir002201@users.noreply.github.com>
2026-02-11 22:47:30 +0600

    product update

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from product.models import Category, Product, Review, ProductImage
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Review)
+admin.site.register(ProductImage)
