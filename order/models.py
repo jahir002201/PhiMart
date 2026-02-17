d6f6dd647ed28ae2eb2fde856ca155635c1e3224
Md. Jahirul Islam <90275453+jahir002201@users.noreply.github.com>
2026-02-17 13:15:21 +0600

    order model update

--- order/models.py
+++ order/models.py
@@ -39,7 +39,7 @@ class Order(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='PENDING')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Not Paid')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
