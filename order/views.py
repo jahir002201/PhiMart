161ed6c13e3b5a17b41b22fdefe5f6412e5aa3b9
Md. Jahirul Islam <90275453+jahir002201@users.noreply.github.com>
2026-02-17 16:31:18 +0600

    order update

--- order/views.py
+++ order/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.http import HttpResponseRedirect
 
-from order.models import Cart, CartItem, Order
+from order.models import Cart, CartItem, Order, OrderItem
 from order.serializers import (
     CartSerializer,
     CartItemSerializer,
@@ -20,6 +20,7 @@ from order.serializers import (
 from order.services import OrderService
 from sslcommerz_lib import SSLCOMMERZ
 from django.conf import settings as main_settings
+from rest_framework.views import APIView
 
 
 
@@ -167,3 +168,12 @@ def payment_cancel(request):
 def payment_fail(request):
     print("Inside fail")
     return HttpResponseRedirect(f"{main_settings.FRONTEND_URL}/dashboard/orders/")
+
+class HasOrderedProduct(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, product_id):
+        user = request.user
+        has_ordered = OrderItem.objects.filter(
+            order__user=user, product_id=product_id).exists()
+        return Response({"hasOrdered": has_ordered})
